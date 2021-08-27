4f0e56355c7f031fe9d9c181e0a4018c69d5bb0a
positive-season <scp94373@gmail.com>
2021-08-27 23:57:30 +0100

    Added guess logic for updating guesses

--- Number Wizard/Assets/NumberWizard.cs
+++ Number Wizard/Assets/NumberWizard.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int maximumNumber = 1000;
+    int minimumNumber = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int maximumNumber = 1000;
-        int minimumNumber = 1;
         
+        //Initial messages
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number");
         Debug.Log($"Highest number is: {maximumNumber}");
         Debug.Log($"Lowest number is: {minimumNumber}");
-        
+        Debug.Log($"Tell me if your number is higher or lower than {guess}");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        //Max number 1001, allowing the max number to be 1000
+        maximumNumber = maximumNumber + 1;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            minimumNumber = guess;
+            //Generate new guess
+            guess = (maximumNumber + minimumNumber) / 2;
+            Debug.Log($"Is it higher or lower than {guess}?");
+        } else if(Input.GetKeyDown(KeyCode.DownArrow)) {
+            maximumNumber = guess;
+            //Generate new guess
+            guess = (maximumNumber + minimumNumber) / 2;
+            Debug.Log($"Is it higher or lower than {guess}?");
+        } else if(Input.GetKeyDown(KeyCode.Return)) {
+            Debug.Log("Excellent, we're on the same wave length!");
+        }
         
     }
 }
